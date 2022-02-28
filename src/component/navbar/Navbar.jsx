559667e18a6c3ef315a885d17f58c21c9fca1358
demiladeore <oluwafemidemi47@gmail.com>
2022-03-01 00:10:40 +0100

    add all newly created folder and files for the project

---
+++ src/component/navbar/Navbar.jsx
@@ -0,0 +1,65 @@
+import React, { useState, Fragment } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import "./navbar.css";
+import logo from "../../assets/logo.svg";
+
+const Menu = () => {
+  return (
+    <Fragment>
+      {" "}
+      <p href="#home">Home</p>
+      <p href="#wgpt3">What is GPT-3</p>
+      <p href="#possibility">Open AI</p>
+      <p href="#features">Case studies</p>
+      <p href="#blog">Libraries</p>
+    </Fragment>
+  );
+};
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign In</p>
+                <button type="button">Sign Up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/container/possibility/Possibility.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Possibility() {
+  return (
+    <div>
+      <h2>Possibility</h2>
+    </div>
+  );
+}
+
+export default Possibility;
